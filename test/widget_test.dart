83fb1d929c2a46bef550ec1b1e557973ce941fbd
Ben Fried <ben.fried@gmail.com>
2020-02-01 12:32:11 -0500

    make up to date

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sudoku/main.dart';
+import 'package:Phoodoku/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
